36db393c98f4d0968e0b240be2387f8036732cc0
aliahmed338 <aliahmed338@gmail.com>
2025-08-26 01:22:23 +0300

    add production url

--- src/app/utils/constant.ts
+++ src/app/utils/constant.ts
@@ -1,5 +1,5 @@
 export const ARTICLE_PER_PAGE = 6;
 
-// export const DOMAIN = "https://article-full-stack-app-next.vercel.app";
+export const DOMAIN = "https://blog-three-red-94.vercel.app/";
 
-export const DOMAIN = "http://localhost:3000";
+// export const DOMAIN = "http://localhost:3000";
