20a289f075c9b8007b6148bad20ac9c56fcce7b6
aliahmed338 <aliahmed338@gmail.com>
2025-08-26 01:25:39 +0300

    edit url

--- src/app/utils/constant.ts
+++ src/app/utils/constant.ts
@@ -1,5 +1,5 @@
 export const ARTICLE_PER_PAGE = 6;
 
-export const DOMAIN = "https://blog-three-red-94.vercel.app/";
+export const DOMAIN = "https://blog-three-red-94.vercel.app";
 
 // export const DOMAIN = "http://localhost:3000";
